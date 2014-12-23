7828e4e1ce5e14b748db065ef89c0e09c9577ab1
Martin Reck <github@martinreck.de>
2014-12-23 18:31:40 +0100

    Shell script for installing Oracle JDK 8 alternatives added.

---
+++ home/pi/install/jvm/install-jdk8-oracle-alternatives.sh
@@ -0,0 +1,5 @@
+update-alternatives --install /usr/bin/jrunscript jrunscript /usr/lib/jvm/jdk-8-oracle-arm-vfp-hflt/bin/jrunscript 100
+update-alternatives --install /usr/bin/schemagen  schemagen  /usr/lib/jvm/jdk-8-oracle-arm-vfp-hflt/bin/schemagen  100
+update-alternatives --install /usr/bin/wsgen      wsgen      /usr/lib/jvm/jdk-8-oracle-arm-vfp-hflt/bin/wsgen      100
+update-alternatives --install /usr/bin/wsimport   wsimport   /usr/lib/jvm/jdk-8-oracle-arm-vfp-hflt/bin/wsimport   100
+update-alternatives --install /usr/bin/xjc        xjc        /usr/lib/jvm/jdk-8-oracle-arm-vfp-hflt/bin/xjc        100
