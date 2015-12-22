affdcd51d1c65b92624147a250f696a86581d254
Martin Reck <github@martinreck.de>
2015-12-22 22:54:49 +0100

    Tinkerforge brick daemon scripts added.

---
+++ tinkerforge/install-brickd-dependencies.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install libusb-1.0-0 libudev0 pm-utils
---
+++ tinkerforge/install-brickd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -N http://download.tinkerforge.com/tools/brickd/linux/brickd_linux_latest_armhf.deb
+sudo dpkg -i brickd_linux_latest_armhf.deb
