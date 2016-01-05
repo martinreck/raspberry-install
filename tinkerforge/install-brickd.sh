9bec0d407dabd258c14921f06ffbe3c29fc132f6
Martin Reck <github@martinreck.de>
2016-01-05 16:26:16 +0000

    sudo removed, packaged updated.

--- tinkerforge/install-brickd.sh
+++ tinkerforge/install-brickd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget -N http://download.tinkerforge.com/tools/brickd/linux/brickd_linux_latest_armhf.deb
-sudo dpkg -i brickd_linux_latest_armhf.deb
+dpkg -i brickd_linux_latest_armhf.deb
